95eb7ed389738feddc9241e4eac87e480282c2f5
gkhell <gpkmyworld@gmail.com>
2023-03-15 15:09:28 +0530

    15/03/2023⠀⎦⎡⠀15:09⠀⎦⎢⠀Wednesday

---
+++ ECommerceSite-Django/ECommerceSite-Django/ecom/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Customer, Product, Orders
+
+
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Orders, OrderAdmin)
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register your models here.
---
+++ ECommerceSite-Django/ECommerceSite-Django/linux_virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/you/Demons/project django/ECommerceSite-Django/ECommerceSite-Django/linux_virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
